3ff061f3eb3850e63325e45460ea3bd64f147964
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-04-03 19:18:30 -0400

    Entities filled in, Account, Category, Order, Product filled out.

--- Meal Ordering API/Entities/order.cs
+++ Meal Ordering API/Entities/order.cs
@@ -2,5 +2,11 @@
 {
     public class Order
     {
+        public List<Product>? products { get; set; }    
+        public int? StoreId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? Id { get; set; }
+        public bool? updated { get; set; } 
+        public string? Status {  get; set; }
     }
 }
--- Meal Ordering API/Entities/product.cs
+++ Meal Ordering API/Entities/product.cs
@@ -2,5 +2,13 @@
 {
     public class Product
     {
+        public string? Name { get; set; }   
+        public float? Cost { get; set; }   
+        public int? Inventory { get; set; }
+        public int? Id { get; set; }
+        public int? CategoryId { get; set; }
+        public int? StoreId { get; set; }
+        public int? Quantity { get; set; }
+        public bool? Available { get; set; }
     }
 }
