cbf461252f11fda85ec77fb534f841ea41b359d8
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-04-04 13:29:50 -0400

    Renamed files... For Islam...

---
+++ Meal Ordering API/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Meal_Ordering_API.Entities
+{
+    public class Account
+    {
+        public int? Id { get; set; }
+        public Guid? ApiKey { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? AccountType { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+    }
+
+}
