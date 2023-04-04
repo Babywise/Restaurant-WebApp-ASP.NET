9d98b5f61938f3695b720c6eb4b7fb44098f9004
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-04-04 02:20:08 -0400

    Block comments over all actions in Account Controller

--- Meal Ordering API/Controllers/AccountController.cs
+++ Meal Ordering API/Controllers/AccountController.cs
@@ -11,6 +11,12 @@ namespace Meal_Ordering_API.Controllers
 {
     public class AccountController : Controller
     {
+        /// <summary>
+        /// Register an account with the API. Takes in an account object. Must have username, password, address filled out at minimum
+        /// Returns 200 OK if good, sets response.headers["Message"] to status message if success or fail for more details
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
         [HttpGet("/API/V1/Account/Register")]
         public string Register(Account account)
         {
@@ -21,6 +27,14 @@ namespace Meal_Ordering_API.Controllers
             return "hi";
           
         }
+        /// <summary>
+        /// Login to an account. Must have Username and Password at minimum.
+        /// Returns a Response.headers["ApiKey"] if success for the account logged into. 
+        /// Else returns 400 
+        ///     With Response.headers["Message"] with a detailed message about what went wrong
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
         [HttpPost("/API/V1/Account/Login")]
         public string Login(Account account)
         {
@@ -29,6 +43,16 @@ namespace Meal_Ordering_API.Controllers
             Response.Headers.UserAgent = "API";
             return JsonSerializer.Serialize(acc);
         }
+
+        /// <summary>
+        /// Edit the fields in an account. Fields left null or "" will be ignored. Only values with data will be compared and changed.
+        /// Returns 200 OK on success
+        ///     Response.headers["Message"] for a message about the sucess or failure
+        /// Returns 400 Bad Request on failure
+        ///     Response.headers["Message"] for a message about the sucess or failure
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
         [HttpPut("/API/V1/Account/Edit")]
         public string Edit(Account account)
         {
