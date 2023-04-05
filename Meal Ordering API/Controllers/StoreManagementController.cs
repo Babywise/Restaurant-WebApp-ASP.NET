614964ea8dfbfc302098772cd4bf77411079822a
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-04-05 17:38:05 -0400

    Store Management Stubs Completed

--- Meal Ordering API/Controllers/StoreManagementController.cs
+++ Meal Ordering API/Controllers/StoreManagementController.cs
@@ -16,10 +16,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPost("/API/V1/StoreManagement/Add")]
         public string Add(Product product,[FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && product != null )
+            {
+                check = true;
+                message = "Add item";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Product is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Removes an item from the store.
@@ -31,10 +56,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPost("/API/V1/StoreManagement/Remove")]
         public string Remove(Product product, [FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && product != null)
+            {
+                check = true;
+                message = "Remove item";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Product is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Edits a product for the store.
@@ -46,10 +96,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPost("/API/V1/StoreManagement/Edit")]
         public string Edit(Product product, [FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && product != null)
+            {
+                check = true;
+                message = "Edit item";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Product is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Adds a category from a string to the given store that links to the ApiKey
@@ -61,10 +136,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPost("/API/V1/StoreManagement/AddCategory")]
         public string AddCategory(string category, [FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && category != null)
+            {
+                check = true;
+                message = "Add Category";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Category is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Removes a category from the store.
@@ -76,10 +176,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPost("/API/V1/StoreManagement/RemoveCategory")]
         public string RemoveCategory(Category category, [FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && category != null)
+            {
+                check = true;
+                message = "Remove Category";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Category is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Edits a category for the given store.
@@ -91,10 +216,35 @@ namespace Meal_Ordering_API.Controllers
         [HttpPut("/API/V1/StoreManagement/EditCategory")]
         public string EditCategory(Category category, [FromHeader] Guid ApiKey)
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
+            bool check = false;
+            string message = "";
+            //verify
+            if (ApiKey != Guid.Empty && category != null)
+            {
+                check = true;
+                message = "Edit Category";
+            }
+            else
+            {
+                check = false;
+                message = "Either Guid is empty or Category is null";
+            }
+
+
+            // Set Headers
             Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            Response.Headers["Message"] = message;
+
+            if (check)
+            {
+                Response.StatusCode = 200;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+            }
+            //return
+            return "";
         }
         /// <summary>
         /// Gets all categories from the store.
@@ -103,12 +253,24 @@ namespace Meal_Ordering_API.Controllers
         /// <param name="ApiKey"></param>
         /// <returns></returns>
         [HttpGet("/API/V1/StoreManagement/getAllCategories")]
-        public string GetAllCategories([FromHeader] Guid ApiKey)
+        public string GetAllCategories()
         {
-            Account acc = new Account();
-            acc.FirstName = "Danny";
-            Response.Headers.UserAgent = "API";
-            return JsonSerializer.Serialize(acc);
+            List<Category> categories = new List<Category>();
+
+            Category category1 = new Category()
+            {
+                Name = "Category1",
+                Id = 1
+            };
+            Category category2 = new Category()
+            {
+                Name = "Category2",
+                Id = 1
+            };
+
+            categories.Add(category1);
+            categories.Add(category2);
+            return JsonSerializer.Serialize(categories);
         }
     }
 }
