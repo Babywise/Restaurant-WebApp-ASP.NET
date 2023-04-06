de9ae4f062c8bf19dea3c58a1a1c17f8e94e1cfb
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-04-06 03:07:48 -0400

    Altered index

--- Meal Ordering API/Controllers/HomeController.cs
+++ Meal Ordering API/Controllers/HomeController.cs
@@ -432,6 +432,31 @@ namespace Meal_Ordering_API.Controllers
 
             return View();
         }
+        public IActionResult placeOrder()
+        {
+
+            // create request to register test
+            HttpWebRequest request = WebRequest.Create("https://localhost:7062/API/V1/Ordering/placeOrder") as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.UserAgent = "StubTest";
+            request.Headers.Add("ApiKey", "{2bcf9c86-dad9-4fee-a884-769dd60f4b0f}");
+
+
+            HttpWebResponse response = null;
+            //Response from api
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception ex)
+            {
+            }
+            string data = Classes.Responses.getKeyFromResponse(response, "Message");
+            //  LoginResponse resp = Classes.Responses.getLoginResponseFromResponse(response); // gets account from string
+
+            return View();
+        }
         public IActionResult Index()
         {
 
