ccf8aec490d33fc27afad600d92f424df6e9c955
Babywise <islamdoesart@gmail.com>
2023-04-16 19:16:04 -0400

    Added Required Tags to Login and Reqist Request

--- Meal Ordering Class Library/RequestEntitiesShared/AccountLoginRequest.cs
+++ Meal Ordering Class Library/RequestEntitiesShared/AccountLoginRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace Meal_Ordering_Class_Library.RequestEntitiesShared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meal_Ordering_Class_Library.RequestEntitiesShared
 {
     public class AccountLoginRequest
     {
+        [Required]
         public string? Username { get; set; }
+        [Required]
         public string? Password { get; set; }
     }
 }
--- Meal Ordering Class Library/RequestEntitiesShared/AccountRegisterRequest.cs
+++ Meal Ordering Class Library/RequestEntitiesShared/AccountRegisterRequest.cs
@@ -1,15 +1,26 @@
-﻿namespace Meal_Ordering_Class_Library.RequestEntitiesShared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meal_Ordering_Class_Library.RequestEntitiesShared
 {
     public class AccountRegisterRequest
     {
+        [Required]
         public string? Username { get; set; }
+        [Required]
         public string? Password { get; set; }
+        [Required]
         public string? ConfirmPassword { get; set; }
+        [Required]
         public string? FirstName { get; set; }
+        [Required]
         public string? LastName { get; set; }
+        [Required]
         public string? AccountType { get; set; }
+        [Required]
         public string? Email { get; set; }
+        [Required]
         public string? PhoneNumber { get; set; }
+        [Required]
         public string? Address { get; set; }
     }
 }
