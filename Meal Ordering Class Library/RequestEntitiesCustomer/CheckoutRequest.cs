39570df5a9d871103e9cad7e32c4ae275f7ac80b
Babywise <islamdoesart@gmail.com>
2023-04-17 00:11:41 -0400

    deleted unused entities & modified LoginRepsonse Entity

--- Meal Ordering Class Library/RequestEntitiesCustomer/CheckoutRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Meal_Ordering_Class_Library.RequestEntitiesCustomer
-{
-    public class CheckoutRequest
-    {
-        //Needs Implementation
-    }
-}
--- Meal Ordering Class Library/RequestEntitiesRestaurant/UpdateOrderRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Meal_Ordering_Class_Library.Entities;
-
-namespace Meal_Ordering_Class_Library.RequestEntitiesRestaurant
-{
-    public class UpdateOrderRequest
-    {
-        public Order? Order { get; set; }
-    }
-}
